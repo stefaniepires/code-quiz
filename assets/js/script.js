d5114ff908e0e574869d62afbd260ab091c57592
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-06 11:43:55 -0600

    added clear interval as well as a placeholder alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,18 @@ let count = 75;
 //this event listener is listening to when the user clicks on the "Let's Go" button, then timer starts
 btnStart.addEventListener ("click", function() {
     //display a number on the page and for every second that passes, that number decreases by 1
-    setInterval(function (){ 
+    const timerId = setInterval(function (){ 
     //this will update the timer text
     //beginning with decreasing count by 1
         count -=1;
     //this will change the html text to be the actual value of count
         timerText.textContent = count;
-
+        if(count === 0) {
+            timerText.textContent = "Time's Up!";
+            clearInterval(timerId);
+            alert("Check your scores to see how you did!")
+        
+        }
 //this means, for every 1 second, perform the above function
     }, 1000);
 
